52102c98732d69827682327215c30cdde12df747
Rumeet Singh <singh.rumeet5@gmail.com>
2021-01-02 16:37:44 +0530

    added upcasting and downcasting

--- Object-Oriented-Programming/S01Inheritance/src/com/company/Main.java
+++ Object-Oriented-Programming/S01Inheritance/src/com/company/Main.java
@@ -3,8 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+	    var ui = new UIControl(true);
 	    var tb = new TextBox();
-	    tb.setText("HelloWorld!");
-        System.out.println(tb);
+	    show(ui);
+    }
+
+    public static void show(UIControl control) {
+        if (control instanceof TextBox) {
+            var textBox = (TextBox)control;
+            textBox.setText("Hello World");
+        }
+        System.out.println(control);
     }
 }
